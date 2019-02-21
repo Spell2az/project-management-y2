902f2297e2cab152329ffcc07150dd8ba775ed59
Spell2az <martin_kovac@outlook.com>
2019-02-21 13:34:44 +0000

    Get Item qunatity from basket

--- Watch Shop Website/MasterPage.master.cs
+++ Watch Shop Website/MasterPage.master.cs
@@ -4,11 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Inventory;
 
-public partial class MasterPage2 : System.Web.UI.MasterPage
+public partial class MasterPage : System.Web.UI.MasterPage
 {
+    public string BasketQuantityLabelText
+    {
+        get => lblBasketQuantity.Text;
+        set => lblBasketQuantity.Text = value;
+    }
     protected void Page_Load(object sender, EventArgs e)
     {
+       
+            var basket = (Basket)Session["Basket"] ?? new Basket();
+            Session["Basket"] = basket;
+            var basketQuantity = basket.GetItemQuantity;
+            lblBasketQuantity.Text = basketQuantity == 0 ? "" : basketQuantity.ToString();
+        
+    }
 
     }
-}
+
+
+
