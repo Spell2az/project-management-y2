f362a14eca77c69871f1453d32dbf82dc3b30e5a
Spell2az <martin_kovac@outlook.com>
2019-03-21 17:03:11 +0000

    Add Login page

---
+++ ManagementApp/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManagementApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Signin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+      public ActionResult Login()
+      {
+        return Redirect("~/Dashboard/");
+      }
+    }
+}
